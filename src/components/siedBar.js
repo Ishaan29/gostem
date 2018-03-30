ed0ecee88610dd1858a5a10ed6aa11ef6280c147
Ishaan29 <bajpaiishan@yahoo.in>
2018-03-30 06:28:43 +0530

    fire

--- src/components/siedBar.js
+++ src/components/siedBar.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import Content from './content';
 import './sideBar.css';
-
+import SignInPage from './SignIn';
+import { auth } from '../firebase';
+import SignIn from './SignIn';
+import {firebase} from '../firebase';
 class SideBar extends Component {
     constructor(props){
         super(props);
         
-        
+        this.state = {
+            authUser: null
+        }
     }
+    componentDidMount (){
+        firebase.auth.onAuthStateChanged(authUser => {
+          authUser
+          ? this.setState(() => ({ authUser}))
+          : this.setState(() => ({authUser: null}));
+        });
+      }
     render() {
+        if(authUser){
         return(
             <div className = "grid">
                 <div className = "navbar">
@@ -19,13 +32,18 @@ class SideBar extends Component {
                     </div>
                 </div>
                 <div className = "sidebar">
-                <button className = "big-btn"> Logout</button>
+                <button className = "big-btn" onClick={auth.doSignOut}> Logout</button>
                 <button className = "nav-btn">Inbox</button>
                 <button className = "nav-btn">New User</button>
                 <button className = "nav-btn">New</button>
                 </div>
                 <Content />
                 
+            </div>
+        );}
+        return(
+            <div>
+                <SignIn/>
             </div>
         );
     }
